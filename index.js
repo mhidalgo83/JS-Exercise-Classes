8b8b02307672c836e8f27b829c4c4baa7220b23c
Matt <makeshifthidalgo@hotmail.com>
2020-02-18 20:36:39 -0700

    passing 46 tests

--- index.js
+++ index.js
@@ -173,6 +173,20 @@ class Student extends Lambdasian {
   constructor(obj) {
     super(obj);
     this.previousBackground = obj.previousBackground;
+    this.className = obj.className;
+    this.favSubjects = obj.favSubjects;
+  }
+
+  listSubjects() {
+    return `Loving ${this.favSubjects}`
+  }
+
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`
+  }
+
+  sprintChallenge(subject) {
+    return`${this.name} has begun sprint challenge on ${subject}`
   }
 }
 
